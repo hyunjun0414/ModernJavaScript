632edc187d7527c658ab5dfae95e1f6c827cc7a5
hyunjun0414 <80450602+hyunjun0414@users.noreply.github.com>
2023-03-16 23:41:01 +0900

    모던자바스크립트 학교 수업 1주차
    ..

---
+++ script.js
@@ -0,0 +1,30 @@
+const arr = [1, 2, 3];
+
+arr.forEach(num => {
+  alert(num);
+});
+
+
+// const arr = [1,2,3];
+// arr.forEach(alert);
+
+
+
+// const arr = [1,2,3];
+
+// function print(v,idx,arr){
+//   // console.log(v,idx,arr);
+//   console.log("arr:",v);
+// }
+// arr.forEach((v)=>console.log("arr :",v));
+// // arr.forEach(print);
+// // arr.forEach(console.log);
+
+// for(let i = 0; i < arr.length;i++){
+//   console.log("arr :",arr[i]);
+// }
+
+
+// console.log('hello');
+// console.log('hello');
+// console.log('hello');
